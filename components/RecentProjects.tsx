7eff9735d019b208b911a9982b8164f3e1c0b396
Vikash Thakur <thakvikash622@gmail.com>
2024-07-26 12:59:04 +0530

    Fixed the background Color

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -40,7 +40,7 @@ const RecentProjects = () => {
                                         ))
                                     }
                                 </div>
-                                <div className="bg-red-500  flex items-center justify-center gap-3 border lg:p-3 rounded-2xl bg-black-100 p-2">
+                                <div className="flex items-center justify-center gap-3 border lg:p-3 rounded-2xl bg-black-100 p-2">
                                     <p className="text-purple text-[10px] md:text-sm">Check Live Site</p>
                                     <FaLocationArrow />
                                 </div>
